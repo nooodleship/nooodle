203f21b848759dcea02d4aa89063a4311d8cd02c
nooodleship <97960376+nooodleship@users.noreply.github.com>
2022-01-18 22:02:44 +0800

    Add files via upload
    https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?a=2927

---
+++ C_MM15.c
@@ -0,0 +1,15 @@
+#include<stdio.h>  
+#include<math.h>  
+  
+int main()  
+{  
+    double t1,t2;  
+    scanf("%lf %lf",&t1,&t2);  
+    if(t1<=100 && t2<=100){  
+            printf("inside\n");  
+          
+    }  
+    else{  
+        printf("outside\n");  
+    }  
+}  
