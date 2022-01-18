ff95a7cf2bac622c5be6945f81afb433a7dc7c62
nooodleship <97960376+nooodleship@users.noreply.github.com>
2022-01-18 22:04:55 +0800

    Add files via upload
    https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?a=15910

---
+++ BASIC06.c
@@ -0,0 +1,21 @@
+#include<stdio.h>  
+  
+int main(){  
+    int n;  
+    scanf("%d",&n);  
+    if(n==12){  
+        printf("Winter\n");  
+    }  
+    else if(n>=1 && n<=2){  
+        printf("Winter\n");  
+    }  
+    else if(n>=3 && n<=5){  
+        printf("Spring\n");  
+    }  
+    else if(n>=6 && n<=8){  
+        printf("Summer\n");  
+    }  
+    else if(n>=9 && n<=11){  
+        printf("Autumn\n");  
+    }  
+}  
